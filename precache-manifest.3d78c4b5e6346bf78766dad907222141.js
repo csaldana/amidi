b3c7ef7f592904e99c13db9edcbb0c1a0a5e23e3
Jonathan <jonathan@messoft.com>
2019-11-23 18:57:10 -0600

    Updates

---
+++ precache-manifest.3d78c4b5e6346bf78766dad907222141.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "738b8aa49c0b971a37d536342dcbdd32",
+    "url": "/amidi/index.html"
+  },
+  {
+    "revision": "318f2a7b95e89082c292",
+    "url": "/amidi/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "61fc81bf4b802642119b",
+    "url": "/amidi/static/js/2.686f3d9e.chunk.js"
+  },
+  {
+    "revision": "318f2a7b95e89082c292",
+    "url": "/amidi/static/js/main.edc24b03.chunk.js"
+  },
+  {
+    "revision": "abe546ab6efc25c8520a",
+    "url": "/amidi/static/js/runtime-main.234a3b0e.js"
+  }
+]);
